1c42a1fa741e7cf61c53711685b26fdfb57e765d
Scott <scott.linderman@gmail.com>
2014-09-10 09:35:32 -0400

    adding subpackages to setup script

--- setup.py
+++ setup.py
@@ -3,10 +3,15 @@
 from distutils.core import setup
 
 setup(name='PyGLM',
-      version='1.0',
+      version='0.1',
       description='Bayesian inference in GLMs for neural spike trains',
       author='Scott Linderman',
       author_email='slinderman@seas.harvard.edu',
       url='http://www.github.com/slinderman/pyglm',
-      packages=['pyglm'],
+      packages=['pyglm',
+                'pyglm.components',
+                'pyglm.inference',
+                'pyglm.models',
+                'pyglm.plotting',
+                'pyglm.utils'],
      )
