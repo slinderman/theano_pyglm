e216c5449d3d48e20b0d6d7756194e4fbd8e786f
Scott <scott.linderman@gmail.com>
2014-09-10 09:27:38 -0400

    adding setup script for easy installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PyGLM',
+      version='1.0',
+      description='Bayesian inference in GLMs for neural spike trains',
+      author='Scott Linderman',
+      author_email='slinderman@seas.harvard.edu',
+      url='http://www.github.com/slinderman/pyglm',
+      packages=['pyglm'],
+     )
