1d1aaafc3ae916dfa2f4264ccf51024a06e2ec60
Scott Linderman <slinderman@seas.harvard.edu>
2014-09-12 15:39:33 -0400

    adding script to compute STA

--- scripts/compute_sta.py
+++ scripts/compute_sta.py
@@ -1,5 +1,9 @@
 import os
 import numpy as np
+
+import matplotlib
+if 'DISPLAY' not in os.environ:
+    matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 from pyglm.utils.io import parse_cmd_line_args, load_data
@@ -38,10 +42,13 @@ plt.figure()
 for n in range(min(N,5)):
     for j,l in enumerate(lags_to_plot):
         plt.subplot(N,len(lags_to_plot), n*len(lags_to_plot) + j + 1)
-        plt.title('N: %d, Lag: %d' % (n, j))
+        # plt.title('N: %d, Lag: %d' % (n, j))
         plt.imshow(np.kron(s[n,l,:].reshape((10,10)), np.ones((10,10))),
                    vmin=-s_max[n], vmax=s_max[n],
                    cmap='RdGy')
+        plt.xlabel(None)
+        plt.ylabel(None)
+
         if j == len(lags_to_plot) - 1:
             plt.colorbar()
 
